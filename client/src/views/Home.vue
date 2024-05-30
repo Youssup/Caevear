794ade8bf9161d52b7617430765842917a15437f
youssup <Youssupsong@gmail.com>
2024-05-30 18:57:47 -0400

    Deletes

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,21 +3,13 @@
 
 <template>
     <div>
-        <h1 class="Title">Oil paintings and charcoal drawings</h1>
+        <h1 class="Title">Painting and ceramics</h1>
         <h2 class="Name">By Vicky Wong</h2>
         <div class="columns is-mobile">
             <div class="column"></div>
             <div class="column is-four-fifths">
                 <div class="Images">
                     <img src="../assets/images/3.jpg" />
-                    <h4>Abstract 1</h4>
-                    <h5>28&quot;x28&quot;</h5>
-                    <img src="../assets/images/1.jpg" />
-                    <h4>Still Life 1</h4>
-                    <h5>18&quot;x24&quot;</h5>
-                    <img src="../assets/images/2.jpg" />
-                    <h4>Still Life 2</h4>
-                    <h5>18&quot;x24&quot;</h5>
                 </div>
             </div>
             <div class="column"></div>
@@ -66,28 +58,6 @@
     height: auto;
 }
 
-.Images h4 {
-    width: 100%;
-    text-align: left;
-    margin-top: 1.5rem;
-    font-family: "Montserrat", Sans-serif;
-    font-size: 30px;
-    font-weight: 500;
-    text-transform: uppercase;
-    letter-spacing: 1.9px;
-}
-
-.Images h5 {
-    width: 100%;
-    text-align: left;
-    margin-top: 1rem;
-    font-family: "Cormorant", Sans-serif;
-    font-size: 30px;
-    font-weight: 550;
-    line-height: 0.2em;
-    letter-spacing: 1.2px;
-    margin-bottom: 7rem;
-}
 
 .InfoContainer {
     display: flex;
